d841a438f50505f1c1e19dec4ffa34962b73e3db
Gesang Paudra J <g.paudra@gmail.com>
2023-06-13 18:27:26 +0700

    Add Main Menu for accessing all features

---
+++ GT-ModernCalculator/MainMenu.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace GT_ModernCalculator
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(12F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(32, 32, 32);
+            ClientSize = new Size(1200, 562);
+            Font = new Font("Ubuntu", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = Color.White;
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "MainMenu";
+            Text = "Main Menu";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
