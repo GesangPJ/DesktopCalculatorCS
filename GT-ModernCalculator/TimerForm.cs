fbd536398a87d48df8cba414bc55afa181b759a5
Gesang Paudra J <g.paudra@gmail.com>
2023-06-13 18:29:30 +0700

    Add Timer Function

--- GT-ModernCalculator/TimerForm.cs
+++ GT-ModernCalculator/TimerForm.cs
@@ -16,7 +16,40 @@ namespace GT_ModernCalculator
         {
             InitializeComponent();
         }
-        
+        private void btnStartTimer_Click(object sender, EventArgs e)
+        {
+            int hours = int.Parse(TxtHour.Text);
+            int minutes = int.Parse(TxtMinute.Text);
+            int seconds = int.Parse(TxtSecond.Text);
+
+            int totalTimeInSeconds = (hours * 3600) + (minutes * 60) + seconds;
+
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // Set the interval to 1 second (1000 milliseconds)
+            timer.Tick += (s, args) =>
+            {
+                if (totalTimeInSeconds > 0)
+                {
+                    totalTimeInSeconds--;
+
+                    int remainingHours = totalTimeInSeconds / 3600;
+                    int remainingMinutes = (totalTimeInSeconds % 3600) / 60;
+                    int remainingSeconds = (totalTimeInSeconds % 60);
+
+                    TxtHour.Text = remainingHours.ToString("D2");
+                    TxtMinute.Text = remainingMinutes.ToString("D2");
+                    TxtSecond.Text = remainingSeconds.ToString("D2");
+                }
+                else
+                {
+                    timer.Stop();
+                    MessageBox.Show("Timer completed!");
+                }
+            };
+
+            timer.Start();
+        }
+
 
     }
 }
