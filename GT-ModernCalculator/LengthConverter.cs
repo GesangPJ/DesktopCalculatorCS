00eacb7d7bd35915d73414709bc8bb4735be96a8
Gesang Paudra J <g.paudra@gmail.com>
2023-06-13 09:52:34 +0700

    Add length converter in switch function type

--- GT-ModernCalculator/LengthConverter.cs
+++ GT-ModernCalculator/LengthConverter.cs
@@ -16,5 +16,42 @@ namespace GT_ModernCalculator
         {
             InitializeComponent();
         }
+        // Length Converter Function
+        private void ConvertLength(double length, string fromUnit, TextBox targetTextBox)
+        {
+            double result = 0;
+            
+            switch (fromUnit.ToLower())
+            {
+                case "milimeter":
+                    result = length / 1000;
+                    break;
+                case "centimeter":
+                    result = length / 100;
+                    break;
+                case "foot":
+                    result = length * 0.3048;
+                    break;
+                case "inch":
+                    result = length * 0.0254;
+                    break;
+                case "meter":
+                    result = length;
+                    break;
+                case "kilometer":
+                    result = length * 1000;
+                    break;
+                case "mile":
+                    result = length * 1609.34;
+                    break;
+                default:
+                    targetTextBox.Text = "";
+                    return;
+            }
+
+
+
+        }
+
     }
 }
