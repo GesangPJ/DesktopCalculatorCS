ae9d3a80baee2da1e4956d6f4ed43eae4fae1d74
Gesang Paudra J <g.paudra@gmail.com>
2023-06-13 18:19:23 +0700

    Timer Form designing

--- GT-ModernCalculator/TimerForm.Designer.cs
+++ GT-ModernCalculator/TimerForm.Designer.cs
@@ -31,47 +31,134 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Ubuntu", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Font = new Font("Ubuntu", 16.1999989F, FontStyle.Regular, GraphicsUnit.Point);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(243, 55);
+            label1.Location = new Point(166, 63);
             label1.Name = "label1";
-            label1.Size = new Size(75, 29);
+            label1.Size = new Size(88, 33);
             label1.TabIndex = 0;
             label1.Text = "Hours";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Ubuntu", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Font = new Font("Ubuntu", 16.1999989F, FontStyle.Regular, GraphicsUnit.Point);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(427, 55);
+            label2.Location = new Point(350, 63);
             label2.Name = "label2";
-            label2.Size = new Size(95, 29);
+            label2.Size = new Size(115, 33);
             label2.TabIndex = 1;
             label2.Text = "Minutes";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Ubuntu", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Font = new Font("Ubuntu", 16.1999989F, FontStyle.Regular, GraphicsUnit.Point);
             label3.ForeColor = Color.White;
-            label3.Location = new Point(631, 55);
+            label3.Location = new Point(554, 63);
             label3.Name = "label3";
-            label3.Size = new Size(100, 29);
+            label3.Size = new Size(117, 33);
             label3.TabIndex = 2;
             label3.Text = "Seconds";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(32, 32, 32);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Ubuntu", 48F, FontStyle.Underline, GraphicsUnit.Point);
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(158, 129);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(96, 92);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "00";
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(32, 32, 32);
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Ubuntu", 48F, FontStyle.Underline, GraphicsUnit.Point);
+            textBox2.ForeColor = Color.White;
+            textBox2.Location = new Point(356, 129);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(96, 92);
+            textBox2.TabIndex = 4;
+            textBox2.Text = "00";
+            textBox2.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(32, 32, 32);
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("Ubuntu", 48F, FontStyle.Underline, GraphicsUnit.Point);
+            textBox3.ForeColor = Color.White;
+            textBox3.Location = new Point(554, 129);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(96, 92);
+            textBox3.TabIndex = 5;
+            textBox3.Text = "00";
+            textBox3.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Ubuntu", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(275, 129);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 93);
+            label4.TabIndex = 6;
+            label4.Text = ":";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Ubuntu", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(473, 129);
+            label5.Name = "label5";
+            label5.Size = new Size(60, 93);
+            label5.TabIndex = 7;
+            label5.Text = ":";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DodgerBlue;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Ubuntu", 17.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(345, 318);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 49);
+            button1.TabIndex = 8;
+            button1.Text = "START";
+            button1.UseVisualStyleBackColor = false;
             // 
             // TimerForm
             // 
             AutoScaleDimensions = new SizeF(12F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(32, 32, 32);
-            ClientSize = new Size(977, 474);
+            ClientSize = new Size(810, 474);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -88,5 +175,11 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
     }
 }
