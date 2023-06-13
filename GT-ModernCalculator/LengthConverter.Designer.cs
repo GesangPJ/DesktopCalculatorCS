ac4a6c96114dd788f26a7cdc5d34522c98720076
Gesang Paudra J <g.paudra@gmail.com>
2023-06-13 09:24:34 +0700

    Populate Length Converter

--- GT-ModernCalculator/LengthConverter.Designer.cs
+++ GT-ModernCalculator/LengthConverter.Designer.cs
@@ -28,21 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(178, 93);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Centimeters";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(178, 42);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Milimiters";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(178, 144);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Inches";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(178, 195);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 25);
+            label4.TabIndex = 3;
+            label4.Text = "Meters";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(178, 246);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 25);
+            label5.TabIndex = 4;
+            label5.Text = "Foot";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(178, 297);
+            label6.Name = "label6";
+            label6.Size = new Size(103, 25);
+            label6.TabIndex = 5;
+            label6.Text = "Kilomters";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(178, 348);
+            label7.Name = "label7";
+            label7.Size = new Size(62, 25);
+            label7.TabIndex = 6;
+            label7.Text = "Miles";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(32, 32, 32);
+            textBox1.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(339, 40);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(166, 27);
+            textBox1.TabIndex = 7;
+            textBox1.Text = "0";
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(32, 32, 32);
+            textBox2.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.ForeColor = Color.White;
+            textBox2.Location = new Point(339, 93);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(166, 27);
+            textBox2.TabIndex = 8;
+            textBox2.Text = "0";
+            textBox2.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(32, 32, 32);
+            textBox3.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.ForeColor = Color.White;
+            textBox3.Location = new Point(339, 144);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(166, 27);
+            textBox3.TabIndex = 9;
+            textBox3.Text = "0";
+            textBox3.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(32, 32, 32);
+            textBox4.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.ForeColor = Color.White;
+            textBox4.Location = new Point(339, 195);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(166, 27);
+            textBox4.TabIndex = 10;
+            textBox4.Text = "0";
+            textBox4.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.FromArgb(32, 32, 32);
+            textBox5.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox5.ForeColor = Color.White;
+            textBox5.Location = new Point(339, 246);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(166, 27);
+            textBox5.TabIndex = 11;
+            textBox5.Text = "0";
+            textBox5.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = Color.FromArgb(32, 32, 32);
+            textBox6.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox6.ForeColor = Color.White;
+            textBox6.Location = new Point(339, 297);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(166, 27);
+            textBox6.TabIndex = 12;
+            textBox6.Text = "0";
+            textBox6.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = Color.FromArgb(32, 32, 32);
+            textBox7.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox7.ForeColor = Color.White;
+            textBox7.Location = new Point(339, 348);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(166, 27);
+            textBox7.TabIndex = 13;
+            textBox7.Text = "0";
+            textBox7.TextAlign = HorizontalAlignment.Right;
             // 
             // LengthConverter
             // 
             AutoScaleDimensions = new SizeF(12F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(32, 32, 32);
-            ClientSize = new Size(1200, 562);
+            ClientSize = new Size(556, 562);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Font = new Font("Ubuntu", 12F, FontStyle.Regular, GraphicsUnit.Point);
             Margin = new Padding(4, 4, 4, 4);
             Name = "LengthConverter";
             Text = "Length Converter mode";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
     }
 }
