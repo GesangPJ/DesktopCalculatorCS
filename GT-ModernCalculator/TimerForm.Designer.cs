47984b6336eb46134120b1e3d2a8096dc20b7d2e
Gesang Paudra J <g.paudra@gmail.com>
2023-06-13 17:53:45 +0700

    Add label to timer form

--- GT-ModernCalculator/TimerForm.Designer.cs
+++ GT-ModernCalculator/TimerForm.Designer.cs
@@ -28,21 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Ubuntu", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(243, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 29);
+            label1.TabIndex = 0;
+            label1.Text = "Hours";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Ubuntu", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(427, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 29);
+            label2.TabIndex = 1;
+            label2.Text = "Minutes";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Ubuntu", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(631, 55);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 29);
+            label3.TabIndex = 2;
+            label3.Text = "Seconds";
             // 
             // TimerForm
             // 
             AutoScaleDimensions = new SizeF(12F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(32, 32, 32);
-            ClientSize = new Size(1200, 562);
+            ClientSize = new Size(977, 474);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Font = new Font("Ubuntu", 12F, FontStyle.Regular, GraphicsUnit.Point);
             Margin = new Padding(4, 4, 4, 4);
             Name = "TimerForm";
             Text = "Timer Mode";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
