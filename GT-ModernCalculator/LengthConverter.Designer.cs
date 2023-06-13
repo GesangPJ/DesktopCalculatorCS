6d48b6e57d6bbca8d4255a157c7d4e93b6546a85
Gesang Paudra J <g.paudra@gmail.com>
2023-06-13 09:15:53 +0700

    Add Textbox to length form

--- GT-ModernCalculator/LengthConverter.Designer.cs
+++ GT-ModernCalculator/LengthConverter.Designer.cs
@@ -35,6 +35,13 @@
             label5 = new Label();
             label6 = new Label();
             label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
             SuspendLayout();
             // 
             // label1
@@ -114,12 +121,103 @@
             label7.TabIndex = 6;
             label7.Text = "mile";
             // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(32, 32, 32);
+            textBox1.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(346, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(163, 27);
+            textBox1.TabIndex = 7;
+            textBox1.Text = "0";
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(32, 32, 32);
+            textBox2.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.ForeColor = Color.White;
+            textBox2.Location = new Point(346, 95);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(163, 27);
+            textBox2.TabIndex = 8;
+            textBox2.Text = "0";
+            textBox2.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(32, 32, 32);
+            textBox3.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.ForeColor = Color.White;
+            textBox3.Location = new Point(346, 141);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(163, 27);
+            textBox3.TabIndex = 9;
+            textBox3.Text = "0";
+            textBox3.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(32, 32, 32);
+            textBox4.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.ForeColor = Color.White;
+            textBox4.Location = new Point(346, 187);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(163, 27);
+            textBox4.TabIndex = 10;
+            textBox4.Text = "0";
+            textBox4.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.FromArgb(32, 32, 32);
+            textBox5.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox5.ForeColor = Color.White;
+            textBox5.Location = new Point(346, 233);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(163, 27);
+            textBox5.TabIndex = 11;
+            textBox5.Text = "0";
+            textBox5.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = Color.FromArgb(32, 32, 32);
+            textBox6.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox6.ForeColor = Color.White;
+            textBox6.Location = new Point(346, 279);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(163, 27);
+            textBox6.TabIndex = 12;
+            textBox6.Text = "0";
+            textBox6.TextAlign = HorizontalAlignment.Right;
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = Color.FromArgb(32, 32, 32);
+            textBox7.Font = new Font("Ubuntu", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox7.ForeColor = Color.White;
+            textBox7.Location = new Point(346, 325);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(163, 27);
+            textBox7.TabIndex = 13;
+            textBox7.Text = "0";
+            textBox7.TextAlign = HorizontalAlignment.Right;
+            // 
             // LengthConverter
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(32, 32, 32);
             ClientSize = new Size(534, 450);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Controls.Add(label7);
             Controls.Add(label6);
             Controls.Add(label5);
@@ -142,5 +240,12 @@
         private Label label5;
         private Label label6;
         private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
     }
 }
